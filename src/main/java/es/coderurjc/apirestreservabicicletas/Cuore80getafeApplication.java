52bd3e96b7af9182c9225032a42808005c8f01d1
raulrodriguezlr <12rulo12@gmail.com>
2022-07-10 20:41:10 +0200

    proyecto creado

---
+++ src/main/java/es/coderurjc/apirestreservabicicletas/Cuore80getafeApplication.java
@@ -0,0 +1,13 @@
+package es.coderurjc.apirestreservabicicletas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cuore80getafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cuore80getafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/coderurjc/apirestreservabicicletas/Cuore80getafeApplicationTests.java
@@ -0,0 +1,13 @@
+package es.coderurjc.apirestreservabicicletas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cuore80getafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
