16d811ba89b759e0bfb65cef571e749ad891d7fe
DvParedes <davidparedesgomez.27@gmail.com>
2022-07-14 19:52:57 +0200

    Commmit 14-07-2022 -Añadido enlaces de las redes sociales, numero de telefono y algun enlace -Clase WebController creada para los mappings, index y about creado

---
+++ src/main/java/es/cuore80getafe/cuore80getafe/WebController.java
@@ -0,0 +1,19 @@
+package es.cuore80getafe.cuore80getafe;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/about")
+	public String about() {
+		return "about";
+	}
+	
+}
